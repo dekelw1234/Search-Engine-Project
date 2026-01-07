0d35324f1d284a482a3bb6a348ac5a4dbea5c094
Sagi Katan <sagikatan9@gmail.com>
2026-01-07 18:30:51 +0200

    build index

--- inverted_index_gcp.py
+++ inverted_index_gcp.py
@@ -13,7 +13,7 @@ from google.cloud import storage
 from collections import defaultdict
 from contextlib import closing
 
-PROJECT_ID = 'YOUR-PROJECT-ID-HERE'
+PROJECT_ID = 'ex3-sagikatan'
 def get_bucket(bucket_name):
     return storage.Client(PROJECT_ID).bucket(bucket_name)
 
